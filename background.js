1d5592efde41370a407be5ec2876c378c1f8185b
Alex Chuchunkov <scarywound@gmail.com>
2015-03-26 00:37:15 +0300

    bugfixes for /search page

--- background.js
+++ background.js
@@ -1,64 +1,75 @@
 $(document).ready(function() {
 
-	// no matter how you call shit, it's still shit..
-	var bad_words = [
-	    'mix', 'bootleg', 'rmx',
-	    'edit', 'twerk', 'refix',
-	    'bootup', 're-rub', 'club', 'rework',
-	    're boot', 'bass', 'retwerk',
-		'mush', 'remaster', 'mash', 'vocal',
-		'boosted', 'version', 'обрезка', 'нарезка',
-		'concept', 'reboot', 'dj'];
+    // no matter how you call shit, it's still shit..
+    var bad_words = [
+        'mix', 'bootleg', 'rmx',
+        'edit', 'twerk', 'refix',
+        'bootup', 're-rub', 'club', 'rework',
+        're boot', 'bass', 'retwerk',
+        'mush', 'remaster', 'mash', 'vocal',
+        'boosted', 'version', 'обрезка', 'нарезка',
+        'concept', 'reboot', 'dj'];
 
-	// string helper
-	var contains = function(str1, str2) {
-		return str1.indexOf(str2) !== -1;
-	};
+    // string helpers
+    var contains = function(str1, str2) {
+        return str1.indexOf(str2) !== -1;
+    };
 
-	var contains_any = function(str, words) {
-		for (i in words) {
-			if (contains(str, words[i])) return true;
-		}
-		return false;
-	};
+    var contains_any = function(str, words) {
+        for (i in words) {
+            if (contains(str, words[i])) return true;
+        }
+        return false;
+    };
 
-	var searching_for_remixes = false;
-	searching_for_remixes = contains_any($("input#s_search").val().toLowerCase(), bad_words);
+    var input = $('input#s_search').length? $('input#s_search') : $('input#search_query');
 
-	// whenever user updates the search term, update
-	// searching_for_remix varible
-	// keyup event might be really slow, but it is used by VK scripts, workarounds are:
-	// - 'change' event (would trigger on Enter, which might be never pressed)
-	// - throttling the 'keyup' handler, which might still allow remixes to sneak in from time to time
-	$("input#s_search").on('keyup', function() {
-		searching_for_remixes = contains_any($(this).val()
-			.toLowerCase(), bad_words);
-		console.log("searching for crazy shit? ", searching_for_remixes);
-	});
+    var searching_for_remixes = false;
+    searching_for_remixes = contains_any(input.val().toLowerCase(), bad_words);
 
-	// whenever the list of audios updates, we traverse the inserted nodes
-	// and delete remixes if necessary
-	var track_handler = function(e) {
+    // whenever user updates the search term, update
+    // searching_for_remix varible
+    // keyup event might be really slow, but it is used by VK scripts, workarounds are:
+    // - 'change' event (would trigger on Enter, which might be never pressed)
+    // - throttling the 'keyup' handler, which might still allow remixes to sneak in from time to time
+    input.on('keyup', function() {
+        searching_for_remixes = contains_any($(this).val()
+            .toLowerCase(), bad_words);
+        console.log("searching for crazy shit? ", searching_for_remixes);
+    });
 
-		// should we even look at this element?
-		if (searching_for_remixes) return false;
-		var target = $(e.target);
-		if (!target.is("div.audio")) return false;
+    // whenever the list of audios updates, we traverse the inserted nodes
+    // and delete remixes if necessary
+    var track_handler = function(element) {
 
-		// okay, grabbing the artist and title..
-		artist = target.find('.title_wrap').find('a').first().text();
-		title = target.find('span.title').first().text();
+        // should we even look at this element?
+        if (searching_for_remixes) return false;
+        var target = $(element);
+        if (!target.is("div.audio")) return false;
 
-		if (contains_any(artist.toLowerCase(), bad_words) ||
-			contains_any(title.toLowerCase(), bad_words)) {
-			console.log(artist, title, ' is a fucking remix! gosh!');
-			target.remove();
-		}
-	};
+        // okay, grabbing the artist and title..
+        artist = target.find('.title_wrap').find('a').first().text();
+        title = target.find('span.title').first().text();
 
-	// main playlist
-	$('#audios_list').on('DOMNodeInserted', track_handler);
+        if (contains_any(artist.toLowerCase(), bad_words) ||
+            contains_any(title.toLowerCase(), bad_words)) {
+            console.log(artist, title, ' is a fucking remix! gosh!');
+            target.remove();
+        }
+    };
 
-	// playlist in the header (added dynamically)
-	$('body').on('DOMNodeInserted', '#pad_playlist_panel', track_handler);
+    // main playlist
+    $('#audios_list').on('DOMNodeInserted', function(e) {
+        return track_handler(e.target);
+    });
+
+    // playlist in the header (added dynamically)
+    $('body').on('DOMNodeInserted', '#pad_playlist_panel', function(e) {
+        return track_handler(e.target);
+    });
+
+    // server-generated playlist
+    $("#results.audio_results").children('div.audio').each(function(index, el) {
+        return track_handler(el);
+    });
 });
